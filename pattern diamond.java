9e5c6caf1b95f54077f4e38ff112d16edf18b84c
CHETAN CHHETRI <chhetrichetan45@gmail.com>
2025-02-16 03:00:37 +0530

    Update pattern diamond.java

--- pattern diamond.java
+++ pattern diamond.java
@@ -1,19 +1,32 @@
 class sample213{
     public static void main(String[] args) {
-        int [] A={2,34,66,1,44};
-        for (int i = 0; i < A.length-1; i++) {
-            for (int j = 0 ; j< A.length; j++){
-                if (A[j]>A[j+1]) {
-                    int S = A[j];
-                    A[j]=A[j+1];
-                    A[j+1]=S;
-                    
-                }
+        int n = 5;              // maximum number of star present in 1 line
+        int space=((n-1)/2);
+        int backspa=n-space;
+        int num=1;
+        // upper half
+        for (int i=0 ; i<((n+1)/2) ;i++){
+            for (int j=0 ;j<space;j++){
+                System.out.print(" ");          //Add space to the pattern
             }
+            for (int star=0 ;star<num ;star++){
+                System.out.print("*");          //add * to the pattern
+            }
+            space-=1;
+            num+=2;
+            System.out.println();
+        }
+        //bottom half
+        for (int k=0;k<((n+1)/2);k++){
+            num-=2;
+            for (int spa=0;spa<k;spa++){
+                System.out.print(" ");
+            }
+            for (int sta=0;sta<num ;sta++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        }
-        for (int i = 0; i < A.length; i++) {
-            System.out.println(A[i]);
-        }
     }
 }
